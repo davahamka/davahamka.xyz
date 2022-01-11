af2ae214c8caaf40c544640244b7eaaddafde00f
davahamka <davahamka512@gmail.com>
2022-01-11 11:41:23 +0700

    fix: type error build

--- components/pages/home/tech-stack-card.tsx
+++ components/pages/home/tech-stack-card.tsx
@@ -10,7 +10,7 @@ const TechStackCard = ({ label, img }: CardProps) => {
     <div className='h-[100px] border cursor-pointer hover:scale-105 duration-150 transition border-[#DFDFDF] dark:border-[#404040] rounded-lg p-2'>
       <div className='flex flex-row items-center h-full px-4 space-x-2'>
         <div className='w-full lg:w-1/2 flex justify-center'>
-          <Image src={img} width='54' height='54' alt={label} />
+          <Image src={img as string} width='54' height='54' alt={label} />
         </div>
         <div className='sm:w-1/2 hidden lg:flex'>{label}</div>
       </div>
